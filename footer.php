8e25d02473d570eb908c05d0502c12197e26b5c1
Alberto Ridolfi <amsridf@gmail.com>
2026-03-29 13:07:37 -0300

    Changed from PHP to static HTML

--- footer.php
+++
@@ -1,9 +0,0 @@
-</main><!-- .container -->
-<footer class="site-footer">
-  <div class="site-footer-inner">
-    <a class="back-link" href="javascript:history.back(1);">← Voltar</a>
-    <p class="disclaimer">Aviso: este website não tem nenhuma associação com nenhuma fabricante ou empresa vendedora de medicamentos. Seu conteúdo é meramente informativo para auxiliar as pessoas com narcolepsia e seus médicos que estão perdidos em relação aos tratamentos disponíveis lá fora mas que aqui pouco se fala.</p>
-  </div>
-</footer>
-</body>
-</html>
